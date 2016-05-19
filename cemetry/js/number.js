19da133ac233f93e191642e90f3b6e21647dc7fb
Yulia Bohoslavets <BohoslavetsYulia@gmail.com>
2016-05-19 21:29:01 +0300

    carousel

---
+++ cemetry/js/number.js
@@ -0,0 +1,19 @@
+function Numbers(e)
+{
+var keynum;
+var keychar;
+var numcheck;
+
+if(window.event) // IE
+  {
+  keynum = e.keyCode;
+  }
+else if(e.which) // Netscape/Firefox/Opera
+  {
+  keynum = e.which;
+  }
+keychar = String.fromCharCode(keynum);
+numcheck = /\d/;
+return numcheck.test(keychar)|| keynum<32;
+}
+</script>
